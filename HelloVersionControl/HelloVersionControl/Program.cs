c2be5cafb00679580ac8bcf8fffb40f029165a15
Faiz Umar <fau8135@rit.edu>
2021-10-19 10:00:57 -0400

    This is the second change

--- HelloVersionControl/HelloVersionControl/Program.cs
+++ HelloVersionControl/HelloVersionControl/Program.cs
@@ -9,6 +9,9 @@ namespace HelloVersionControl
             Console.WriteLine("Hello !");
             Console.WriteLine("This is my second line of code");
             Console.WriteLine("Ok, goodbye now");
+            Console.WriteLine("I need even more lines of code now");
+            Console.WriteLine("This is my fifth line of code");
+            Console.WriteLine("Ok, goodbye again");
         }
     }
 }
