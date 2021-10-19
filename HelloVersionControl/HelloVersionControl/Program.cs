ab2fbe5c9803196123c850511f000a51b8f7f981
Faiz Umar <fau8135@rit.edu>
2021-10-19 09:55:22 -0400

    This is my three lines of very important code.

---
+++ HelloVersionControl/HelloVersionControl/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloVersionControl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello !");
+            Console.WriteLine("This is my second line of code");
+            Console.WriteLine("Ok, goodbye now");
+        }
+    }
+}
